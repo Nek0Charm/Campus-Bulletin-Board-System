0c09979d62a86d979a56ca90d3be110205735adc
Nek0Charm <2404849517@qq.com>
2026-06-08 15:54:01 +0800

    fix(db): merge alembic multiple heads

---
+++ backend/migrations/versions/5b0af807c62a_merge_heads.py
@@ -0,0 +1,26 @@
+"""merge_heads
+
+Revision ID: 5b0af807c62a
+Revises: 4a2e8f1b3c6d, 5f6a7b8c9d10
+Create Date: 2026-06-08 15:48:25.808814
+
+"""
+
+from typing import Sequence, Union
+
+
+# revision identifiers, used by Alembic.
+revision: str = "5b0af807c62a"
+down_revision: Union[str, Sequence[str], None] = ("4a2e8f1b3c6d", "5f6a7b8c9d10")
+branch_labels: Union[str, Sequence[str], None] = None
+depends_on: Union[str, Sequence[str], None] = None
+
+
+def upgrade() -> None:
+    """Upgrade schema."""
+    pass
+
+
+def downgrade() -> None:
+    """Downgrade schema."""
+    pass
